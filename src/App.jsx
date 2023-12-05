60a94108a7ada58009b1ca6a631e2645a3ce1d37
Mustafa hamza <mustafa@Mustafa-MacBook-Air-3.local>
2023-12-05 17:20:52 +0300

    React Kodları

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import vitelogo from "/vite.svg"
+import './style.css'
+function App() {
+
+  return (
+    <div className='App'>
+      merhaba
+      <br />
+      <img src= {vitelogo} alt="logo" />
+    </div>
+  )
+}
+
+export default App
